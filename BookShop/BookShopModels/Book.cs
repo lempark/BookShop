5a20d833ceee52bf6948b46426968f8c960497b8
SIB <wentres.34@gmail.com>
2020-01-22 14:42:45 +0200

    Added first version of CRUD

---
+++ BookShop/BookShopModels/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BookShopModels
+{
+    public class Book : IBook
+    {
+        public double Price { get; set; }
+
+        public string Name { get; set; }
+              
+        public string Author { get; set; }
+    }
+}
---
+++ BookShop/BookShopModels/Interfaces/IBook.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public interface IBook
+{
+	double Price { get; set; }
+
+	string Name { get; set; }
+
+	string Author { get; set; }
+}
